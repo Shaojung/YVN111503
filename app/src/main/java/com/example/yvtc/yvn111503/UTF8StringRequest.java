ff8655bba94ab0bba71c603b1ccb22e70253436b
shaojung <shaojung@gmail.com>
2016-11-15 21:00:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvn111503/UTF8StringRequest.java
@@ -0,0 +1,32 @@
+package com.example.yvtc.yvn111503;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by yvtc on 2016/11/15.
+ */
+
+public class UTF8StringRequest extends StringRequest {
+    public UTF8StringRequest(String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(url, listener, errorListener);
+    }
+
+    @Override
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        String utf8String = null;
+        try {
+            utf8String = new String(response.data, "UTF-8");
+            return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return super.parseNetworkResponse(response);
+    }
+}
